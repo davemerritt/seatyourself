56c9fe4c510a5b669f2a5f468162a4987c76588b
davemerritt <davidmerritt6@gmail.com>
2013-11-18 19:45:14 -0500

    Attempt #3, restaurant controller

---
+++ seatyourself/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Seatyourself::Application.config.secret_key_base = '3d67127d58ed8d851098c6cb258feb6e587e0e3e2122372f8218504c946c9fe1992b8bc2e67ce75ac7d9faeff68573338d3b33d101799848e8d60cfac03b57ac'
